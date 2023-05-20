b57d86d18600ac4d6a8de6458f383b212099fcde
Name <email@example.ru>
2023-05-20 16:17:44 +0300

    Add Login-logick

--- Models/Repository/IRepository.cs
+++ Models/Repository/IRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace MySocialNetwork.Models.Repository
+﻿using System.
+
+namespace MySocialNetwork.Models.Repository
 {
     public interface IRepository<TEntity> where TEntity : class
     {
+        IEnumerable<T> GetAll();
+        T Get(int id);
+        void Create(T item);
+        void Update(T item);
+        void Delete(int id);
     }
 }
--- Models/Repository/IUnitOfWork.cs
+++ Models/Repository/IUnitOfWork.cs
@@ -1,6 +1,9 @@
-﻿namespace MySocialNetwork.Models
+﻿namespace MySocialNetwork.Models.Repository
 {
-    public interface IUnitOfWork
+    public interface IUnitOfWork : IDisposable
     {
+        int SaveChanges(bool ensureAutoHistory = false);
+
+        IRepository<TEntity> GetRepository<TEntity>(bool hasCustomRepository = true) where TEntity : class;
     }
 }
